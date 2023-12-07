176d4a00cb832d6dd0dedccccef8d48911796fab
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2023-12-07 15:52:08 +0900

    소셜 로그인 공급자에 koreanName 추가

--- src/main/java/io/mohajistudio/tangerine/prototype/enums/Provider.java
+++ src/main/java/io/mohajistudio/tangerine/prototype/enums/Provider.java
@@ -1,15 +1,20 @@
 package io.mohajistudio.tangerine.prototype.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Provider {
-    KAKAO("kakao"),
-    NAVER("naver"),
-    GOOGLE("google"),
-    APPLE("apple");
+    KAKAO("kakao", "카카오"),
+    NAVER("naver", "네이버"),
+    GOOGLE("google", "구글"),
+    APPLE("apple", "애플");
 
     private final String value;
+    private final String koreanName;
 
-    Provider(String value) {
+    Provider(String value, String koreanName) {
         this.value = value;
+        this.koreanName = koreanName;
     }
 
     public static Provider fromValue(String value) {
